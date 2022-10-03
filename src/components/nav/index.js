7cdf990ef270f593947a129649c0b95108dd2242
Austin Davis <austindavis@Austins-MBP.attlocal.net>
2022-10-03 00:33:24 -0400

    about and nav components

--- src/components/nav/index.js
+++ src/components/nav/index.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import Nav from './components/Nav';
 import About from './components/About';
 
 function App() {
-
   return (
     <div>
+      <Nav></Nav>
       <main>
         <About></About>
       </main>
