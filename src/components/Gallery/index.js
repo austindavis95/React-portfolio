eb747d5a006e5be03999d2d729029a01d2b6423a
Austin Davis <austindavis@Austins-MBP.attlocal.net>
2022-10-04 18:12:21 -0400

    app update

--- src/components/Gallery/index.js
+++ src/components/Gallery/index.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { capitalizeFirstLetter } from '../../utils/helpers';
+import photo from "../../assets/small/commercial/0.jpg";
 
 function Gallery(props) {
   const currentCategory = {
@@ -11,6 +12,13 @@ function Gallery(props) {
     <section>
       <h1>{capitalizeFirstLetter(currentCategory.name)}</h1>
       <p>{currentCategory.name}</p>
+      <div className="flex-row">
+          <img
+            src={photo}
+            alt="Commercial Example"
+            className="img-thumbnail mx-1"
+          />
+      </div>
     </section>
   );
 }
--- src/components/nav/index.js
+++ src/components/nav/index.js
@@ -1,46 +1,54 @@
-import React from "react";
+import React, { useState } from "react";
 import { capitalizeFirstLetter } from "../../utils/helpers";
 
 function Nav() {
-  const  categories = [
-    { name: 'commercial', description: 'Photos of grocery stores, food trucks, and other commercial projects' },
-    { name: 'portraits', description: 'Portraits of people in my life' },
-    { name: 'food', description: 'Delicious delicacies' },
-    { name: 'landscape', description: 'Fields, farmhouses, waterfalls, and the beauty of nature' }
-  ];
-
-  const handleClick = () => {
-    console.log("click handled")
-  }
+const [categories] = useState([
+  {
+    name: 'commercial',
+    description: 'Photos of grocery stores, food trucks, and other commercial projects',
+  },
+  { name: 'portraits', description: 'Portraits of people in my life' },
+  { name: 'food', description: 'Delicious delicacies' },
+  { name: 'landscape', description: 'Fields, farmhouses, waterfalls, and the beauty of nature' },
+]);
+const [currentCategory, setCurrentCategory] = useState(categories[0]);
 
   return (
-    <header data-testid="header" className="flex-row px-1">
+    <header className="flex-row px-1">
       <h2>
-        <a href="/">
-          <span role="img" aria-label="camera"> 📸</span> Oh Snap!
+        <a data-testid="link" href="/">
+          <span role="img" aria-label="camera">
+            {" "}
+            📸
+          </span>{" "}
+          Oh Snap!
         </a>
       </h2>
       <nav>
         <ul className="flex-row">
           <li className="mx-2">
-            <a href="#about" onClick={() => handleClick()}>
+            <a
+              href="#about"
+            >
               About me
             </a>
           </li>
-          <li className={"mx-2"}>
-            <span onClick={() => handleClick()}>
-              Contact
-            </span>
+          <li>
+            <span>Contact</span>
           </li>
-          {
-            categories.map((category) => (
-              <li className="mx-1" key={category.name} >
-                <span onClick={() => { handleClick(); }}>
-                 {capitalizeFirstLetter(category.name)}
-                </span>
-              </li>
-            ))
-          }
+          {categories.map((category) => (
+            <li className={`mx-1 ${
+                currentCategory.name === category.name && 'navActive'
+                }`} key={category.name}>
+              <span
+                onClick={() => {
+                  setCurrentCategory(category)
+                }}
+              >
+                {capitalizeFirstLetter(category.name)}
+              </span>
+            </li>
+          ))}
         </ul>
       </nav>
     </header>
