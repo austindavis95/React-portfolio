66b6b4a9ea24eae2424bb32ef6fc839a38a44eac
Austin Davis <austindavis@Austins-MBP.attlocal.net>
2022-10-04 12:34:12 -0400

    test

---
+++ src/components/about/__test__/index.test.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { render, cleanup } from '@testing-library/react';
+import '@testing-library/jest-dom/extend-expect';
+import About from '..';
+
+afterEach(cleanup);
+
+describe('About component', () => {
+    // First Test
+    it('renders', () => {
+        render(<About />);
+      });
+
+  // Second Test
+  })
+
--- src/components/about/index.js
+++ src/components/about/index.js
@@ -17,3 +17,4 @@ function About() {
 }
 
 export default About
+
